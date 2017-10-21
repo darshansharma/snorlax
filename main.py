58ca5f92e2aa2506745f7dc8c27b9d7b8f1f1a7c
Darshan Sharma <thedarshansharma@gmail.com>
2017-10-22 02:20:24 +0530

    added wiki support

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import textwrap
 import sys
 
 
-query = str(sys.argv[1])
+query = str(sys.argv[2])
+site = str(sys.argv[1])
 print query
-print "fetching information "+query
-r=requests.get("https://en.wikipedia.org/wiki/%s"%query)
-html_doc=r.text
-soup=BeautifulSoup(html_doc, 'html.parser')
-#print soup
-for p in soup.find_all('p'):
-	#print textwrap.fill(p.get_text())
-        #print textwrap.dedent(p.get_text()).strip()
-        print textwrap.fill(textwrap.dedent(p.get_text()).strip(), initial_indent='', subsequent_indent='    ')
+print "fetching information "+query+" from "+site+"."
+if(site.lower()=="wiki"):
+	r=requests.get("https://en.wikipedia.org/wiki/%s"%query)
+	html_doc=r.text
+	soup=BeautifulSoup(html_doc, 'html.parser')
+	print soup
+	'''for p in soup.find_all('p'):
+		#print textwrap.fill(p.get_text())
+        	#print textwrap.dedent(p.get_text()).strip()
+        	print textwrap.fill(textwrap.dedent(p.get_text()).strip(), initial_indent='', subsequent_indent='    ')
 
+'''
 
 
