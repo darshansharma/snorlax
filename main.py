67a9b655a5cc6c7e4f19e176ba1049e40f9018b3
rudy3333 <mmax3163@gmail.com>
2023-10-04 20:08:04 +0200

    error handling d+_+b

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ import textwrap
 import sys
 import wikipedia
 
+# Check if the user provided enough command-line arguments
+if len(sys.argv) < 3:
+    print("Usage: python main.py <site> <query>\nExample: python main.py wikipedia pwd")
+    sys.exit(1)
 
-query = str(sys.argv[2])
 site = str(sys.argv[1])
-print("Want to know about the command : " + query)
-print("Fetching information for the command "+query+" from "+site+".")
-if(site == "wikipedia" or site == "wiki"):
-	
-    result = wikipedia.summary("'%s' linux command" %query, sentences = 7)
-    print(result)
-                      
-# soup=BeautifulSoup(html_doc, 'html.parser')
-# print(soup)
-'''for p in soup.find_all('p'):
-            # print textwrap.fill(p.get_text())
-            # print textwrap.dedent(p.get_text()).strip()
-            print textwrap.fill(textwrap.dedent(p.get_text()).strip(), initial_indent='', subsequent_indent='    ')
+query = str(sys.argv[2])
+print("Want to know about the command: " + query)
+print("Fetching information for the command " + query + " from " + site + ".")
 
-'''
+if site == "wikipedia" or site == "wiki":
+    try:
+        result = wikipedia.summary(f"'{query}' linux command", sentences=7)
+        print(result)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(f"DisambiguationError: {e}")
+    except wikipedia.exceptions.PageError as e:
+        print(f"PageError: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+else:
+    print("Invalid site. Please use 'wikipedia' or 'wiki' as the site.")
